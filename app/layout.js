b0f6085f6a0e4f7d5a0b7010561a76fbec834697
Standajagermaker <stanislav.jager@gmail.com>
2026-04-28 17:55:13 +0200

    Add Leaflet CSS

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,12 @@ export const metadata = { title: "Scammer Safari" };
 export default function RootLayout(props) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        />
+      </head>
       <body>{props.children}</body>
     </html>
   );
